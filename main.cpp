657f173f1bead0f96a419bdede34537423a95cec
bastien <bastiendel33@gmail.com>
2024-03-22 14:11:50 +0100

    [ADD] Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file main.cpp
+ * @author Bastien, Victor, AlexisR 
+ * @brief Main file for the project
+ * @version 0.1
+ * @date 2024-03-22
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
